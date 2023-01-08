2fe52753d49c80b3f48f7408ca1d826b2cdd0dae
Clann_Home:김홍제_집 <restoreclann@gmail.com>
2023-01-09 03:46:18 +0900

    21th_Column_row

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, avoid_print
+// ignore_for_file: prefer_const_constructors, avoid_print, prefer_const_literals_to_create_immutables
 // ignore_for_file: use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
@@ -8,7 +8,6 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,22 +24,65 @@ class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      backgroundColor: Colors.teal,
       body: SafeArea(
-        child: Container(
-          width: 100,
-          height: 100,
-          margin: EdgeInsets.symmetric(
-            vertical: 50,
-            horizontal: 10,
-          ),
-          padding: EdgeInsets.all(20),
-          color: Colors.blue[200],
-          child: Text("data"),
+        child: Row(
+          // verticalDirection: VerticalDirection.down,
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          // crossAxisAlignment: CrossAxisAlignment.stretch,
+          // mainAxisSize: MainAxisSize.min,
+          children: [
+            /* 
+              가로 세로 100px 
+              색상 [화이트 블루 레드]
+              각각의 container 이름 부여
+            */
+            MyContainer1(),
+            MyContainer1(
+              flexWidth: 100,
+              color: Colors.blue,
+            ),
+            SizedBox(
+              width: 100,
+              height: 100,
+            ),
+            MyContainer1(),
+          ],
         ),
       ),
     );
   }
 }
 
-// Container는 Page 내에서 최대한 많은 공간을 차지하는 특징이 있습니다.
+// class MyContainer1 extends StatelessWidget { 매개변수 String color 추가
+class MyContainer1 extends StatelessWidget {
+  final int flexWidth;
+  final Color color;
+  const MyContainer1({
+    super.key,
+    this.color = Colors.white,
+    this.flexWidth = 100,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: flexWidth.toDouble(),
+      height: 100,
+      color: color,
+      child: Text('white'),
+    );
+  }
+}
+
+// vscode 가 가볍고 편한것이 장점
+// andStudio 다양하고 효율적인 기능과 안정적인것이 장점
+/* 
+  * column widget은 세로축 방향으로 가능한 모든 공간을 차지한다.
+  * new Keywords
+  1. verticalDirection
+  2. mainAxisAlignment
+  3. crossAxisAlignment
+  4. mainAxisSize
+ */
+
